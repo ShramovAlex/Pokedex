4b92f8b0081c170de6c41185f777ea030e2f68a5
Shramov Alexander <shramov.alexandr@gmail.com>
2022-01-15 17:15:39 +0200

    initial setup

---
+++ app/src/main/java/com/shramov/pokedex/ui/theme/Shape.kt
@@ -0,0 +1,15 @@
+package com.shramov.pokedex.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+/**
+ * @author Alexandr Shramov
+ * @date 15.01.2022
+ */
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
